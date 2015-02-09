716243ecd958c566cc84a70b4fd2542983e589dc
alliecraw <abc41@pitt.edu>
2015-02-08 21:33:43 -0500

    changed background to pitt
    attempted to have google users login, but it’s commented out

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 import os
 import webapp2
 from google.appengine.ext.webapp import template
-
+from google.appengine.api import users
 
 def render_template(handler, templatename, templatevalues) :
   path = os.path.join(os.path.dirname(__file__), 'templates/' + templatename)
@@ -12,6 +12,16 @@ def render_template(handler, templatename, templatevalues) :
 class MainPage(webapp2.RequestHandler) :
   def get(self) :
     render_template(self, 'index.html', {})
+  #   user = users.get_current_user()
+  #   logout_url = ''
+  #   login_url = ''
+
+  #   if user:
+  #     logout_url = users.get_logout_url('/')
+  #     response.out.write('To log out go to' + logout_url)
+	 # else:
+	 #   login_url = users.get_login_url('/')
+	 #   response.out.write('to login goto' + login_url)
     
 
 app = webapp2.WSGIApplication([
