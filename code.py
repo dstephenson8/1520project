14b504e5b0df88c77d57e369b2f7e6449dc38625
dstephenson8 <d.stephenson8@gmail.com>
2015-02-16 17:08:59 -0500

    Updated Code Almost fully working

--- code.py
+++ code.py
@@ -1,14 +1,56 @@
 import os
+import time
 import webapp2
-from google.appengine.ext.webapp import template
-from google.appengine.api import users
 
-def render_template(handler, templatename, templatevalues) :
+from google.appengine.api import users
+from google.appengine.ext import db
+from google.appengine.ext.webapp import template
+
+###############################################################################
+# We'll just use this convenience function to retrieve and render a template.
+def render_template(handler, templatename, templatevalues):
   path = os.path.join(os.path.dirname(__file__), 'templates/' + templatename)
   html = template.render(path, templatevalues)
   handler.response.out.write(html)
 
+
+###############################################################################
+class MessagePost(db.Model):
+  depart = db.StringProperty()
+  arrive = db.StringProperty()
+  timeofdepart = db.StringProperty()
+  seats = db.StringProperty()
+  message_text = db.StringProperty(multiline=True)
+  time = db.IntegerProperty()
+  user = db.StringProperty()
   
+  # we will use this method to automatically output a formatted time string.
+  def formatted_time(self):
+    return time.ctime(self.time)
+  
+
+###############################################################################
+# This handler will be used to set up the post form at post.html.
+###############################################################################
+class PostPage(webapp2.RequestHandler):
+  def get(self):
+    user = users.get_current_user()
+    login = users.create_login_url('/')
+    logout = users.create_logout_url('/')
+    template_values = {
+      'login': login,
+      'logout': logout,
+      'user': user
+    }
+    render_template(self, 'post.html', template_values)
+    
+
+###############################################################################
+# This is our main page handler.  It will show the MessagePost objects in 
+# the index.html page.
+###############################################################################
+
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
         # Checks for active Google account session
@@ -19,42 +61,62 @@ class MainPage(webapp2.RequestHandler):
         else:
             self.redirect(users.create_login_url(self.request.uri))
 
-class ProcessForm(webapp2.RequestHandler):
-  def post(self):
-    render_template(self, 'infoform.html', {})
 
 
-class ProcessForm2(webapp2.RequestHandler):
-  def post(self):
-    fname = self.request.get('fname')
-    lname = self.request.get('lname')
-    phonenumber = self.request.get('phonenumber')
-    email = self.request.get('email')
-    prefer_contact = self.request.get('prefer_contact')
-    fname = self.request.get('fname')
-    year = self.request.get('year')
-    availseats = self.request.get('availseats')
-    departcity = self.request.get('departcity')
-    arrivecity = self.request.get('arrivecity')
-    time = self.request.get('time')
 
-   
-    render_template(self, 'day.html',{
-     "fname": fname,
-     "lname": lname
-    # "mynumber2": number2,
-    #"number3": number3
-  })
-  
+
+class MainPage2(webapp2.RequestHandler):
   def get(self):
-	render_template(self, 'day.html', {})
+    user = users.get_current_user()
+    login = users.create_login_url('/')
+    logout = users.create_logout_url('/')
+    posts = list()
+    query = MessagePost.all()
+    query.order('-time')
+    for post in query.run():
+      posts.append(post)
+    
+    template_values = {
+      'login': login,
+      'logout': logout,
+      'user': user,
+      'posts': posts
+    }
+    render_template(self, 'day2.html', template_values)
+
+    
+###############################################################################
+# This will handle the form submission, then redirect the user to the main 
+# page.
+###############################################################################
+class SavePostPage(webapp2.RequestHandler):
+  def post(self):
+    user = users.get_current_user()
+    if user:
+      post = MessagePost()
+      post.depart = self.request.get('depart')
+      post.arrive = self.request.get('arrive')
+      post.timeofdepart = self.request.get('timeofdepart')
+      post.seats = self.request.get('seats')
 
 
-
+      post.message_text = self.request.get('text')
+      post.user = user.email()
+      post.time = int(time.time())
+      post.put()
+    self.redirect('/')
+    
+###############################################################################
+# We have to make sure we map our HTTP request pages to the actual
+# RequestHandler objects here.
+#
+# Note that the first argument to WSGIApplication is an array of tuples; each
+# one of these represents one URL-to-RequestHandler pairing.
+###############################################################################
 app = webapp2.WSGIApplication([
   ('/', MainPage),
-  ('/processform', ProcessForm),
-  ('/processform2', ProcessForm2),
-  ('/list_rides', ProcessForm2),
-], debug = True)
+  ('/list_rides', MainPage2),
+  ('/savepost', SavePostPage),
+  ('/post', PostPage)
+], debug=True)
 
