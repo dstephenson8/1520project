e72c672d12d9862bc0b687779ac17467c785f351
Allison Crawford <abc41@pitt.edu>
2015-04-08 17:14:45 -0400

    added some js stuff

---
+++ js/reserve.js
@@ -0,0 +1,29 @@
+// $(document).ready(function(){
+
+var button=element.find("#seatSpan");
+
+    function reserveSeat(seats){
+            //make reservation text green and bold and change button to text
+            $(button, this).html('Seat Reserved');
+            $("#seatSpan", this).css('color', '#00FF00');
+            $("#seatSpan", this).css('fontWeight', 'bold');
+
+
+            //when reserve os clicked, reserve the seat
+            // button="";
+
+            // button='<div onclick="reserve(this)" class="reserved-tag"></div>';
+
+
+
+            // alert(seats);
+            
+            // //decrement deat number in vehicle
+            // var element = seats;
+            // elementNumber = parseFloat(element.innerHTML);
+            // elementNumber-1;
+            // element.InnerHTML = elementNumber;
+        }
+
+
+// });
